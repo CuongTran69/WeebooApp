75acc1c9ef8d1f8cfe518a4f26eb5b05eba2708f
Cường Trần <cuongtv91@ghtk.co>
2024-02-23 15:58:25 +0700

    Initial Commit

---
+++ WeeBoo/WeeBooApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeBooApp.swift
+//  WeeBoo
+//
+//  Created by Cường Trần on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeeBooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
